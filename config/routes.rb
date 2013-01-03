2a9618e1e13179467440ef329fbc995f455b2b3b
sajeevzacharias4 <sajeev.zacharias4@gmail.com>
2013-01-03 12:26:05 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp1::Application.routes.draw do
  
 
+  get "users/new"
+
+match '/signup', :to=>'users#new'
+
 match '/contact', :to=>'pages#contact'
 match '/about', :to=>'pages#about'
 match '/help', :to=>'pages#help'
+root :to=>'pages#home'
 
 
   # The priority is based upon order of creation:
@@ -56,7 +61,7 @@ match '/help', :to=>'pages#help'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-root :to=>'pages#home'
+
 
   # See how all your routes lay out with "rake routes"
 
